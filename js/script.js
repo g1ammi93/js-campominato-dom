bafd16bd30c37c2ce251002dc1d8fed399a437d1
g1ammi93 <giammariacarelli93@hotmail.it>
2024-01-11 20:19:30 +0000

    Added Score

--- js/script.js
+++ js/script.js
@@ -15,18 +15,38 @@ const createCell = (content) => {
     return cell;
 }
 
+// Funzione creazione bombe
+
+const casualBombs = (cells, totalBombs) => {
+    const bomb = [];
+    while (bomb.lenght < totalBombs) {
+        const randomNumber = Math.floor(Math.random() * cells) + 1;
+        if (!bomb.includes(randomNumber)) bomb.push(randomNumber);
+    }
+    return bomb;
+}
 
 // Rows and Cols
 
 const rows = 10;
 const cols = 10;
 const totalCells = rows * cols;
-let score = 0
+// Numero di bombe
+const totalBombs = 16;
+// Punteggio massimo
+const maxPoints = totalCells - totalBombs;
+// Preparo il GameOver
+let gameOver = false;
 
 
 // Svolgimento delle funzioni dentro il click del bottone
 
 button.addEventListener('click', function () {
+
+    // Preparo il punteggio
+    let score = 0;
+    // Scrivo il punteggio in pagina
+    playerScore.innerHTML = `Score: 0`;
 
     // Rimuovo il figlio di grid così che il loop riparta da capo
     while (grid.firstChild) {
@@ -45,16 +65,23 @@ button.addEventListener('click', function () {
 
         cell.addEventListener('click', () => {
             // Rendiamo le celle cliccabili una sola volta
-            if (cell.classList.contains('clicked')) {
-                return;
-            }
+            if (cell.classList.contains('clicked')) return;
+            // Aggiungiamo la classe clicked
             cell.classList.add('clicked');
-            // Aggiugno il punteggio dell'utente
-            ++score
+            // Modifico ed incremento il punteggio in pagina
+            playerScore.innerHTML = `Score: ${++score}`
             // Quando clicco su una cella metto il numero in console
             console.log('Cella cliccata numero:', i)
-            playerScore.innerHTML = `Score: ${score}`
+            // Controllo che non sia stata colpita una bomba
+            const hasHitBomb = bombs.includes(parseInt(cell.innerText));
+
+            // Aggiungo il gameover in caso di vittoria o di bomba colpita
         })
+
+        // generiamo le bombe
+
+        const bombs = casualBombs(totalCells, totalBombs);
+        console.log(bombs)
 
         // Appendo la classe in pagina
 
@@ -64,4 +91,3 @@ button.addEventListener('click', function () {
 
 
 // Svolgiamo il centro del gioco
-
